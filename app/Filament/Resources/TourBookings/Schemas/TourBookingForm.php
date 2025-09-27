182bb98edba08784c34e409a2af787dec437dd4b
aryndraa <aryacoder1102@gmail.com>
2025-09-28 00:51:49 +0800

    Tour booking schema

--- app/Filament/Resources/TourBookings/Schemas/TourBookingForm.php
+++ app/Filament/Resources/TourBookings/Schemas/TourBookingForm.php
@@ -4,8 +4,12 @@ namespace App\Filament\Resources\TourBookings\Schemas;
 
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
 
 class TourBookingForm
 {
@@ -13,24 +17,60 @@ class TourBookingForm
     {
         return $schema
             ->components([
-                Select::make('package_id')
-                    ->relationship('package', 'name')
-                    ->required(),
-                TextInput::make('customer_name')
-                    ->required(),
-                TextInput::make('customer_phone')
-                    ->tel()
-                    ->required(),
-                TextInput::make('customer_email')
-                    ->email()
-                    ->required(),
-                DatePicker::make('booking_date')
-                    ->required(),
-                TextInput::make('people_amount')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('status')
-                    ->default('pending'),
-            ]);
+                Group::make([
+                    Section::make([
+                        Select::make('status')
+                            ->options([
+                                'pending' => 'Pending',
+                                'confirmed' => 'Confirmed',
+                                'cancelled' => 'Cancelled',
+                                'completed' => 'Completed',
+                            ])
+                            ->default('pending')
+                            ->required()
+                            ->native(false)
+                            ->disablePlaceholderSelection()
+                            ->columnSpanFull()
+                            ->columnSpanFull(),
+                    ]),
+                    Section::make([
+                        TextInput::make('customer_name')
+                            ->required()
+                            ->columnSpanFull()
+                            ->disabled(),
+                        TextInput::make('customer_phone')
+                            ->tel()
+                            ->required()
+                            ->disabled(),
+                        TextInput::make('customer_email')
+                            ->email()
+                            ->required()
+                            ->disabled(),
+                        DatePicker::make('booking_date')
+                            ->required()
+                            ->disabled(),
+                        TextInput::make('people_amount')
+                            ->required()
+                            ->numeric()
+                            ->disabled(),
+                    ])
+                    ->columns(2),
+                ])
+                ->columnSpan(3),
+
+
+                Section::make([
+                    TextInput::make('name')
+                        ->label('Package')
+                        ->disabled(),
+                    SpatieMediaLibraryFileUpload::make('image')
+                        ->label(" ")
+                        ->collection('packages')
+                        ->disabled()
+                ])
+                ->relationship('package')
+                ->columnSpan(2)
+            ])
+            ->columns(5);
     }
 }
