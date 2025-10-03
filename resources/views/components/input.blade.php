b94af75664466a734acc2dd74bdbc8ed0ceb5b91
LuxOrionCoding <gungryan1234@gmail.com>
2025-10-03 22:52:55 +0800

    🚧 Progressing on package detail page (book form), 💄 Small visual adjustment on component

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,19 +1,32 @@
 @props([
-    'label' => '',
+    'label' => null,
     'type' => 'text',
     'placeholder' => '',
+    'id' => null,
+    'name' => null,
+    'value' => null,
+    'required' => false,
 ])
 
-<div {{ $attributes->merge([
-    'class' => 'flex flex-1 flex-col p-4 bg-gray-100 font-inter shadow-sm',
-]) }}>
-    <label class="text-sm text-gray-500 mb-2">{{ $label }}</label>
+<div
+    {{ $attributes->merge([
+        'class' => 'flex flex-1 flex-col p-4 bg-gray-100 font-inter shadow-sm rounded-md',
+    ]) }}>
+    @if ($label && $id)
+        <label for="{{ $id }}" class="text-sm text-gray-500 mb-2">
+            {{ $label }}
+        </label>
+    @endif
 
     <div class="flex gap-4 items-center w-full">
         {{ $slot }}
 
-        <input type="{{ $type }}" placeholder="{{ $placeholder }}"
-            {{ $attributes->only(['name', 'id', 'required', 'value']) }}
-            class="bg-none w-full pb-1 border-b-2 border-b-gray-400/50 text-xl text-black font-medium placeholder:text-gray-400/50 placeholder:italic focus:outline-0">
+        <input type="{{ $type }}" id="{{ $id }}" name="{{ $name }}" value="{{ $value }}"
+            placeholder="{{ $placeholder }}" @if ($required) required @endif
+            @if ($type === 'tel') pattern="^\+?[0-9\s\-().]{7,20}$" @endif
+            {{ $attributes->except(['class', 'id', 'name', 'value', 'required']) }}
+            class="bg-transparent w-full text-xl text-black font-medium
+                   placeholder:text-gray-400/50 placeholder:italic
+                   focus:outline-0">
     </div>
 </div>
--- resources/views/services/package-detail.blade.php
+++ resources/views/services/package-detail.blade.php
@@ -191,4 +191,135 @@
         </div>
     </section>
 
+    {{-- ? PACKAGE DETAIL SECTION --}}
+    <section class="bg-gray-200/80">
+        <div class="container mx-auto py-16 px-8 [&_h4]:font-roboto [&_h4]:font-medium [&_h4]:text-xl">
+
+            <h2
+                class="relative mb-6 z-10 w-fit font-roboto text-2xl font-semibold text-cst-green-800 after:absolute after:-inset-x-2 after:h-3 after:top-1/2 after:-translate-y-1/2 after:rounded-full after:transition after:bg-cst-yellow-400/40 after:isolate after:-z-10">
+                Package Details</h2>
+
+            <p class="font-inter text-justify leading-normal mb-6">
+                Lorem ipsum dolor sit amet consectetur. Vulputate quis sed pellentesque non metus nullam dapibus. Elit diam
+                vel porttitor bibendum condimentum vulputate sed. Ultrices molestie vel interdum et turpis. Blandit elit non
+                aliquam enim in diam faucibus. Praesent nulla turpis nunc pulvinar dui fusce cursus in. Aliquam feugiat
+                suspendisse tempor urna leo ipsum arcu. Sodales vel erat vitae dictum morbi pretium varius imperdiet ante.
+            </p>
+
+            <h4 class="mb-2">Destinations</h4>
+
+            <ul class="list-disc pl-5 mb-6">
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem, ipsum dolor.</li>
+            </ul>
+
+            <h4 class="mb-2">Included</h4>
+            <ul class="space-y-2 mb-6">
+                <li class="flex items-start gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6 text-green-600 shrink-0">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Lorem ipsum dolor sit.</span>
+                </li>
+                <li class="flex items-start gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6 text-green-600 shrink-0">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Lorem ipsum dolor sit.</span>
+                </li>
+                <li class="flex items-start gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6 text-green-600 shrink-0">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Lorem ipsum dolor sit amet.</span>
+                </li>
+                <li class="flex items-start gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6 text-green-600 shrink-0">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Lorem, ipsum dolor.</span>
+                </li>
+            </ul>
+
+            <h4 class="mb-2">Excluded</h4>
+            <ul class="space-y-2">
+                <li class="flex items-start gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6 text-red-600 shrink-0">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Lorem ipsum dolor sit.</span>
+                </li>
+                <li class="flex items-start gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6 text-red-600 shrink-0">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Lorem ipsum dolor sit.</span>
+                </li>
+            </ul>
+
+            {{-- Start time and Price --}}
+            <div class="mt-8 mb-8 flex w-full flex-wrap gap-4">
+                <div class="bg-cst-green-400 flex flex-1 items-center gap-4 px-5 py-4 rounded-md w-fit">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                        class="size-12 text-cst-yellow-400">
+                        <path fill-rule="evenodd"
+                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+                            clip-rule="evenodd" />
+                    </svg>
+
+                    <div class="font-inter">
+                        <p class="italic text-gray-300">Start Time</p>
+                        <h5 class="text-white text-2xl font-medium">09.00 <b>AM</b></h5>
+                    </div>
+                </div>
+
+                <div class="bg-cst-green-400 flex flex-1 items-center gap-4 px-5 py-4 rounded-md w-fit">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                        class="size-12 text-cst-yellow-400">
+                        <path
+                            d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z" />
+                        <path fill-rule="evenodd"
+                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z"
+                            clip-rule="evenodd" />
+                    </svg>
+
+                    <div class="font-inter">
+                        <p class="italic text-gray-300">Price</p>
+                        <h5 class="text-white text-2xl font-normal"><b>AUD</b> $50</h5>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Book Form --}}
+            <form action="" class="bg-white p-4 font-inter">
+                @csrf
+                <h4 class="font-roboto font-semibold text-4xl text-center mb-5">Book Form</h4>
+
+                <div class="flex flex-col gap-3 [&>div]:p-3">
+                    <x-input id="full-name" name="fullname" label="Full name" placeholder="Jacob Holden" type="text"
+                        required />
+                    <x-input id="phone" name="phone" label="Phone" placeholder="+00 112 3456 7890"
+                        type="tel" required />
+                    <x-input id="email" name="email" label="Email" placeholder="test@example.com" type="email"
+                        required />
+                </div>
+            </form>
+
+        </div>
+    </section>
+
 @endsection
